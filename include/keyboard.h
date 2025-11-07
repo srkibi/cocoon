14cae6c62bbe1aad547e7ae1217f2462aa7a0adb
KIBI <springbonnie034@gmail.com>
2025-11-07 12:19:15 -0300

    Redoing

--- include/keyboard.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-void keyboard_init();
-char keyboard_getchar();
-int keyboard_has_input();
-
-#endif
--- include/wget.h
+++
@@ -1,7 +0,0 @@
-#ifndef WGET_H
-#define WGET_H
-
-int wget_download(const char *url, const char *output_file);
-void wget_show_help();
-
-#endif
